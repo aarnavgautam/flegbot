61ce736e2d9006b58a054ed1f5f070bea64a3d99
aarnavgautam <146917741+aarnavgautam@users.noreply.github.com>
2023-11-06 13:02:50 -0500

    fixed

--- main.py
+++ main.py
@@ -6,26 +6,32 @@ from datetime import datetime, timedelta
 import time
 import json
 
+# open file with username, password, start/end times, first/last names
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
-
+# options to set up chrome to run faster
 options = webdriver.ChromeOptions()
 # options.add_argument('--start_maximized')
 options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
 # Initialize the web browser
 driver = webdriver.Chrome(options=options)
 
-
+# continuously running
 while True:
+    # checks the time
     current_time = datetime.now().time()
 
+    # if its 2:05, then execute login script
     if current_time.hour == 14 and current_time.minute == 5 and current_time.second == 0:
         login_url = 'https://login.ufl.edu'
         driver.get(login_url)
         login_wait = WebDriverWait(driver, 5)
 
+        # input username and password
         username_input = driver.find_element(By.ID, 'username')
         username = config.get('username')
         username_input.send_keys(username)
@@ -37,15 +43,19 @@ while True:
         submit_button = driver.find_element(By.ID, 'submit')
         submit_button.click()
 
+        # duo push
         iframe = driver.find_element(By.ID, 'duo_iframe')
         driver.switch_to.frame(iframe)
 
+        # remember for 10 hrs
         remember_button = driver.find_element(By.NAME, 'dampen_choice')
         remember_button.click()
 
+        # pushes to phone
         duo_push = driver.find_element(By.CLASS_NAME, 'auth-button')
         duo_push.click()
 
+    # books room at exactly 12:00:00 AM
     if current_time.hour == 0 and current_time.minute == 0 and current_time.second == 0:
         # Define the desired date and times
         current_date = datetime.now()
@@ -62,7 +72,6 @@ while True:
 
         # Navigate to the booking URL
         driver.get(booking_url)
-        # time.sleep(1)
         # Wait for the list of available rooms to load
         wait = WebDriverWait(driver, 3)
         # Perform "Ctrl + F" to open the search bar
@@ -76,8 +85,8 @@ while True:
         search_input.send_keys(Keys.RETURN)
         book_now_button = driver.find_element(By.CLASS_NAME, 's-lc-suggestion-book-now')
         book_now_button.click()
-        time.sleep(1)
 
+        # input booking info
         first_name_input = driver.find_element(By.ID, 'fname')
         first_name = config.get('first_name')
         first_name_input.send_keys(first_name)
@@ -88,9 +97,10 @@ while True:
 
         checkbox = driver.find_element(By.ID, 'terms')
         checkbox.click()
-        # time.sleep(1)
 
+        # book the room
         final_submit = driver.find_element(By.ID, 'btn-form-submit')
         final_submit.click()
+
         # Close the browser when done
         driver.quit()
