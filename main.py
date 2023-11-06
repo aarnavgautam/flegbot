1ffb069964b1efa2ab82098ba48b2d91dd65da10
aarnavgautam <146917741+aarnavgautam@users.noreply.github.com>
2023-11-06 11:57:27 -0500

    updated to allow for duo push up to 9 hours and 55 minutes before, and now continuously runs

--- main.py
+++ main.py
@@ -9,81 +9,88 @@ import json
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
+
 options = webdriver.ChromeOptions()
-options.add_argument('--start_maximized')
-'''
-replace with this instead of start maximized
+# options.add_argument('--start_maximized')
 options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-'''
 
 # Initialize the web browser
 driver = webdriver.Chrome(options=options)
 
-# Define the desired date and times
-current_date = datetime.now()
-two_weeks_later = current_date + timedelta(days=14)
-formatted_date = two_weeks_later.strftime('%Y-%m-%d')
-booking_date = formatted_date
-start_time = config.get('start_time')
-end_time = config.get('end_time')
 
-# Construct the booking URL with the desired date and times
-booking_url = f'https://libcal.uflib.ufl.edu/spaces?m=t&lid=4316&gid=7502&capacity=0&date={booking_date}&date-end={booking_date}&start={start_time}&end={end_time}'
+while True:
+    current_time = datetime.now().time()
 
-# Navigate to the booking URL
-driver.get(booking_url)
-# time.sleep(1)
-# Wait for the list of available rooms to load
-wait = WebDriverWait(driver, 10)
-# Perform "Ctrl + F" to open the search bar
-driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 'f')
-# Enter the search term into the search bar
-search_term = "L118 - Diode"
-search_input = driver.switch_to.active_element
-search_input.send_keys(search_term)
+    if current_time.hour == 14 and current_time.minute == 5 and current_time.second == 0:
+        login_url = 'https://login.ufl.edu'
+        driver.get(login_url)
+        login_wait = WebDriverWait(driver, 5)
 
-# Press Enter to start the search
-search_input.send_keys(Keys.RETURN)
-# time.sleep(1)
-book_now_button = driver.find_element(By.CLASS_NAME, 's-lc-suggestion-book-now')
-book_now_button.click()
-time.sleep(10)
+        username_input = driver.find_element(By.ID, 'username')
+        username = config.get('username')
+        username_input.send_keys(username)
 
-username_input = driver.find_element(By.ID, 'username')
-username = config.get('username')
-username_input.send_keys(username)
+        password_input = driver.find_element(By.ID, 'password')
+        password = config.get('password')
+        password_input.send_keys(password)
 
-password_input = driver.find_element(By.ID, 'password')
-password = config.get('password')
-password_input.send_keys(password)
-# time.sleep(3)
-submit_button = driver.find_element(By.ID, 'submit')
-submit_button.click()
+        submit_button = driver.find_element(By.ID, 'submit')
+        submit_button.click()
 
-iframe = driver.find_element(By.ID, 'duo_iframe')
-driver.switch_to.frame(iframe)
-# <button tabindex="2" type="submit" class="positive auth-button"><!-- -->Send Me a Push </button>
-# time.sleep(5)
-time.sleep(5)
-duo_push = driver.find_element(By.CLASS_NAME, 'auth-button')
-duo_push.click()
-time.sleep(10)
-driver.switch_to.default_content()
+        iframe = driver.find_element(By.ID, 'duo_iframe')
+        driver.switch_to.frame(iframe)
 
-first_name_input = driver.find_element(By.ID, 'fname')
-first_name = 'Aarnav'
-first_name_input.send_keys(first_name)
+        remember_button = driver.find_element(By.NAME, 'dampen_choice')
+        remember_button.click()
 
-last_name_input = driver.find_element(By.ID, 'lname')
-last_name = 'Gautam'
-last_name_input.send_keys(last_name)
+        duo_push = driver.find_element(By.CLASS_NAME, 'auth-button')
+        duo_push.click()
 
-checkbox = driver.find_element(By.ID, 'terms')
-checkbox.click()
-time.sleep(3)
+    if current_time.hour == 0 and current_time.minute == 0 and current_time.second == 0:
+        # Define the desired date and times
+        current_date = datetime.now()
 
-final_submit = driver.find_element(By.ID, 'btn-form-submit')
-final_submit.click()
-# Close the browser when done
-driver.quit()
+        two_weeks_later = current_date + timedelta(days=14)
+        formatted_date = two_weeks_later.strftime('%Y-%m-%d')
+        booking_date = formatted_date
+        start_time = config.get('start_time')
+        end_time = config.get('end_time')
+
+        # Construct the booking URL with the desired date and times
+        booking_url = (f'https://libcal.uflib.ufl.edu/spaces?m=t&lid=4316&gid=7502&'
+                       f'capacity=0&date={booking_date}&date-end={booking_date}&start={start_time}&end={end_time}')
+
+        # Navigate to the booking URL
+        driver.get(booking_url)
+        # time.sleep(1)
+        # Wait for the list of available rooms to load
+        wait = WebDriverWait(driver, 3)
+        # Perform "Ctrl + F" to open the search bar
+        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 'f')
+        # Enter the search term into the search bar
+        search_term = "L118 - Diode"
+        search_input = driver.switch_to.active_element
+        search_input.send_keys(search_term)
+
+        # Press Enter to start the search
+        search_input.send_keys(Keys.RETURN)
+        book_now_button = driver.find_element(By.CLASS_NAME, 's-lc-suggestion-book-now')
+        book_now_button.click()
+        time.sleep(1)
+
+        first_name_input = driver.find_element(By.ID, 'fname')
+        first_name = config.get('first_name')
+        first_name_input.send_keys(first_name)
+
+        last_name_input = driver.find_element(By.ID, 'lname')
+        last_name = config.get('last_name')
+        last_name_input.send_keys(last_name)
+
+        checkbox = driver.find_element(By.ID, 'terms')
+        checkbox.click()
+        # time.sleep(1)
+
+        final_submit = driver.find_element(By.ID, 'btn-form-submit')
+        final_submit.click()
+        # Close the browser when done
+        driver.quit()
